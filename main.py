039ac72f6751c9a7c97ce157db92f2616c3f417f
Cruz-9E <adamjayzen222@yahoo.com>
2025-09-21 20:16:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def average_score(event):
     average = int((score1 + score2)/2) # This makes it so that the average rounds off, not giving a floating point
     passing_score = 75
     
-    if average >= passing_score: # Setting up the equation to check whether they passed or failed.
+    if average >= passing_score: # Setting up the equation to check whether they passed or failed
         status = "Passed"
     else:
         status = "Failed"
         
-    display(f"Average: {average}", target="output1")
+    display(f"Average: {average}", target="output1") # F-string so it doesn't just give the number
     display(status, target = "output2")
